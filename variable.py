eb8871dbdde2729b6b6cc6923a41ed6fa6cac263
Arindam Pandey <arindampandey63@gmail.com>
2025-12-11 22:49:09 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+a = 6 # a is an interger type variable
+
+b = 5.99 # b is floating type variable 
+
+c = "Hello" # c is a string
+
+d = False # d is a boolean variable 
+
+e = None # e is a none type variable
