17c94be6fff72cf176cad15340e63c0d49bac335
Arindam Pandey <arindampandey63@gmail.com>
2025-12-12 19:10:29 +0530

    Add files via upload

---
+++ problem8.py
@@ -0,0 +1,6 @@
+# WAP to find out average of two numbers entered by the user 
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = float((a+b)/2)
+
+print("Average =" , c)
