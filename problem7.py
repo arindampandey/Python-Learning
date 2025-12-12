ce06fc1119ad372812965ec332464c5f0d915c73
Arindam Pandey <arindampandey63@gmail.com>
2025-12-12 19:10:16 +0530

    Add files via upload

---
+++ problem7.py
@@ -0,0 +1,5 @@
+# Using comparision operator find out whether a given variable a is greater then b or not 
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b :"))
+
+print("a is greater than b is :", a>b)
