71442355e25bf4b647ea65c97bd57b5d20e6ac73
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:50:46 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,7 @@
+# Tuple is a immutable datatype like strings
+a = () # Empty tuple
+a = (3,1,6,9,False,"Rohan",2.98) # Multiple element tuple
+a = (2,) # Tuple with single element
+
+print(type(a))
+
