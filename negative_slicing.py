41589d34d933cc61ed7289bd6c07ef52348a6f77
Arindam Pandey <arindampandey63@gmail.com>
2025-12-13 15:18:30 +0530

    Add files via upload

---
+++ negative_slicing.py
@@ -0,0 +1,10 @@
+name = "Arindam"
+
+print(name[0:3])
+
+print(name[-4:-1])
+print(name[3:6])
+
+print(name[:4]) # Is same as print(name[0:4])
+print(name[1:]) # Is same as print(name[1:7])
+print(name[1:7])
