6638c08c19cd31cdca78a9b208f45d9a611ecba8
Arindam Pandey <arindampandey63@gmail.com>
2025-12-13 15:19:47 +0530

    Add files via upload

---
+++ problem13.py
@@ -0,0 +1,6 @@
+# WAP to replace double space by single space in a string
+name = "Hello my name is  Ram !!"
+
+print(name.replace("  "," "))
+print(name) # Orignal string never changes as the strings are immutable
+
