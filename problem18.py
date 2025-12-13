be51db47ac9a5b647aa5ba8e54f0619c70ff762d
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:54:35 +0530

    Add files via upload

---
+++ problem18.py
@@ -0,0 +1,10 @@
+# WAP to create a dictionary of hindi words with values as their english translation
+words = {
+    "Madad" : "Help",
+    "Kaise" : "How",
+    "Billi" : "Cat"
+}
+
+word = input("Enter the word you want meaning of : ")
+
+print(words[word])
