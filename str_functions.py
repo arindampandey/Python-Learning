8e81c147a09537ab5a7e6785d94b3e08e1b0ac6a
Arindam Pandey <arindampandey63@gmail.com>
2025-12-13 15:18:45 +0530

    Add files via upload

---
+++ str_functions.py
@@ -0,0 +1,10 @@
+name = "hello"
+
+print(len(name))
+print(name.endswith("llo"))
+print(name.startswith("he"))
+print(name.capitalize())
+
+a = "Good Good work"
+b = a.replace("Good","Bad")
+print(b)                                            
