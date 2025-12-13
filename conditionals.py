d8fe00a1d0d35b2f6c4dfd6274468808c05f81b7
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:56:04 +0530

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your age : "))
+
+if(a>=18):
+    print("You are above the age of consent !!")
+    print("Good for you...")
+
+elif(a<=0):
+    print("Invalid age !")
+
+else:
+    print("You are below the age of consent !!")
+
+print("End of program")
+
