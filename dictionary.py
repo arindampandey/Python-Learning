6a75180737bd94ac17c5d330602e41e7b870dd0e
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:52:54 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# Dictionary is a mutable datatype
+d = {} # Empty Dictionary
+marks = {
+    "Ram" : 45,
+    "Shyam" : 99,
+    "Rohan" : 23,
+    "list" : [1,2,3]
+}
+
+print(type(marks))
+print(marks)
+
+print(marks["Rohan"])
+print(marks["list"])
