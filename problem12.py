ab22100fa0bb8668d0b3166e32bf61d269fb4a42
Arindam Pandey <arindampandey63@gmail.com>
2025-12-13 15:19:33 +0530

    Add files via upload

---
+++ problem12.py
@@ -0,0 +1,5 @@
+# WAP to detect double space in a string
+name = "Hello my name is  Ram !!"
+
+print(name.find("  "))
+# It will return -1 if there is no double space and the index number if a double space is there 
