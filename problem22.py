5ce213c8f21d0a6f5ff28fbfac5d70baa6f2d012
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:56:30 +0530

    Add files via upload

---
+++ problem22.py
@@ -0,0 +1,17 @@
+# WAP to find greatest of 4 numbers entered by the user 
+a = int(input("Enter number 1 : "))
+b = int(input("Enter number 2 : "))
+c = int(input("Enter number 3 : "))
+d = int(input("Enter number 4 : "))
+
+if(a>b and a>c and a>d):
+    print("Greatest number is ",a)
+
+elif(b>a and b>c and b>d):
+    print("Greatest number is ",b)
+
+elif(c>a and c>b and c>d):
+    print("Greatest number is ",c)
+
+else:
+    print("Greatest number is ",d)
