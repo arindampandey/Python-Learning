2baba87b13ee99dce04e92d46f2eae65934bd5d7
Arindam Pandey <arindampandey63@gmail.com>
2025-12-14 00:57:18 +0530

    Add files via upload

---
+++ problem26.py
@@ -0,0 +1,10 @@
+# WAP which finds out whether a given name is present in a list or not.
+l = ["Rohan","Ram","Harish","Nobita"]
+
+name = input("Enter your name : ")
+
+if(name in l):
+    print("The name is present in the list")
+
+else:
+    print("Name is not there")
