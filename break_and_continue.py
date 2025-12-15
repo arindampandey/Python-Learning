264fd23c53df7b84fc153fe9df4b89e5a5555982
Arindam Pandey <arindampandey63@gmail.com>
2025-12-15 19:21:56 +0530

    Add files via upload

---
+++ break_and_continue.py
@@ -0,0 +1,10 @@
+for i in range(100):
+    if(i==33):
+        break # Exits the loop right now
+    print(i)
+
+for i in range(100):
+    if(i==33):
+        continue # Skips this specific iteration
+    print(i)
+
