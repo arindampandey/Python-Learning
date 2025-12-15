7813a4ff90ee157e2a076a7b96b511e5514b2033
Arindam Pandey <arindampandey63@gmail.com>
2025-12-15 19:22:49 +0530

    Add files via upload

---
+++ problem30.py
@@ -0,0 +1,6 @@
+# WAP to greet a person in a list whose name starts with letter 'S'.
+l = ["Rohan","Soham","Shubham","Adarsh"]
+
+for name in l:
+    if(name.startswith("S")):
+        print(f"Hello {name}")
