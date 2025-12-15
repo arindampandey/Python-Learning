345caea738b5246789a17dedbe9723e60953898e
Arindam Pandey <arindampandey63@gmail.com>
2025-12-16 00:24:47 +0530

    Add files via upload

---
+++ problem39.py
@@ -0,0 +1,11 @@
+# WAP using functions to find greatest of hree numbers.
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>a and c>b):
+        return c
+    
+n = greatest(2,5,9)
+print(n)
