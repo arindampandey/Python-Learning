e62ce8443d7e2a00345b664032307992485260f9
Arindam Pandey <arindampandey63@gmail.com>
2025-12-15 19:24:08 +0530

    Add files via upload

---
+++ problem33.py
@@ -0,0 +1,10 @@
+# WAP to find sum of n natural numbers using while loop.
+n = int(input("Enter the value of n : "))
+
+i = 0
+sum = 0
+while(i<=n):
+    sum = sum + i
+    i += 1
+
+print(f"Sum = {sum}")
