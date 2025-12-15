0f803bc6837311be37a1a0112e06d2f25f405030
Arindam Pandey <arindampandey63@gmail.com>
2025-12-16 00:23:37 +0530

    Add files via upload

---
+++ functions_with_arguments.py
@@ -0,0 +1,10 @@
+def greet(name , ending):
+    print(f"Have a good day {name} ! , {ending}")
+    return "Done"
+
+greet("Rohan","Thankyou !!")
+greet("Soham","Thankyou !!")
+
+a = greet("Divya","Thankssss !!")
+
+print(a)
