9fa27bf8cf54a214a50dd7d20c29acbb58f680b4
Arindam Pandey <arindampandey63@gmail.com>
2025-12-15 19:21:30 +0530

    Add files via upload

---
+++ for_loop_iterate.py
@@ -0,0 +1,14 @@
+# For loop with lists
+l = [1,4,6,332,134]
+for i in l:
+    print(i)
+
+# For loop with tuples
+t = (1,3,2,9)
+for i in t:
+    print(i)
+
+# For loop with strings 
+s = "Hello"
+for i in s:
+    print(i)
