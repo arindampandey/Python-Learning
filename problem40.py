6e46e192894517439ab9fdce288f0d044f2e8841
Arindam Pandey <arindampandey63@gmail.com>
2025-12-16 00:25:09 +0530

    Add files via upload

---
+++ problem40.py
@@ -0,0 +1,7 @@
+# WAP using function to convert from celcius to fahrenheit.
+def F_to_C(f):
+    return 5*(f-32)/9
+
+f = int(input("Enter temperature in F: "))
+c = F_to_C(f)
+print(f"{round(c , 2)} °C")
