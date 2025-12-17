317b056ae81e7f0599466f071e0d05106316114f
Arindam Pandey <arindampandey63@gmail.com>
2025-12-17 17:58:32 +0530

    Add files via upload

---
+++ 2_file_write.py
@@ -0,0 +1,7 @@
+str = "Heyy , what is your name ?"
+
+f = open("myfile.txt" , "w")
+f.write(str)
+f.close()
+
+# The file with name "myfile.txt" will be created and the content inside the string str will be written in it.
