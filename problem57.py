d81b2ea94509ff978b862b8a167069a4c99553b1
Arindam Pandey <arindampandey63@gmail.com>
2025-12-17 18:02:35 +0530

    Add files via upload

---
+++ problem57.py
@@ -0,0 +1,6 @@
+# ⁠Write a python program to rename a file to "renamed_by_python.txt.
+with open("file.txt") as f:
+    content = f.read()
+
+with open("renamed_by_python.txt" , "w") as f:
+    f.write(content)
