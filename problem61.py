422f6f1ae371e67ab8d908e04321af7461edfccf
Arindam Pandey <arindampandey63@gmail.com>
2025-12-31 19:36:43 +0530

    Update problem61.py

--- problem61.py
+++ problem61.py
@@ -1,4 +1,4 @@
-# Write a class Train which has methods to book a ticket, get status (no of seats) and get fare information of train running under Indian Railways.
+# Write a class Train which has methods to book a ticket, get status (no of seats) and get fare information of train running under Indian Railways .
 from random import randint
 
 class Train:
