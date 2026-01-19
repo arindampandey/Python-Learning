fbf96c942d12ec24269dc4896bfb258cac704ce6
Arindam Pandey <arindampandey63@gmail.com>
2026-01-19 23:35:50 +0530

    Add files via upload

---
+++ 11_list_comprehensions.py
@@ -0,0 +1,11 @@
+mylist = [2,3,9,7,6]
+
+# squarelist = []
+# for item in mylist:
+#     squarelist.append(item*item)
+
+# This can be done by using ...
+
+squarelist = [i*i for i in mylist]
+
+print(squarelist)
