effec3de6dac36757940f690ae42254db941c8ca
Arindam Pandey <arindampandey63@gmail.com>
2026-01-19 23:35:28 +0530

    Add files via upload

---
+++ 10_enumerate.py
@@ -0,0 +1,12 @@
+l = [11,23,42,78]
+
+# index = 0
+# for item in l:
+#     print(f"The item number at index {index} is {item}")
+#     index += 1
+
+# This can be done by using enumerate functiom
+
+for index , item in enumerate(l):
+    print(f"The item number at index {index} is {item}")
+    
